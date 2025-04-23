0dae0aed558d3393a66c3904e0dd3a0403f964b9
ritwik vinay <ritwikvinay38@gmail.com>
2025-04-23 09:45:10 +0530

    Initial Commit-MMT complete

--- src/test/java/org/testrv/ex11_SeleniumActions/TestSelenium_ActionClass_MMT1.java
+++ src/test/java/org/testrv/ex11_SeleniumActions/TestSelenium_ActionClass_MMT1.java
@@ -33,30 +33,66 @@ public class TestSelenium_ActionClass_MMT1 extends TestSele_Start_End_Browser {
                 .sendKeys("Mum")
                 .build()
                 .perform();
-
+        checkvisibility(driver, By.xpath("//ul[@class='react-autosuggest__suggestions-list']/li"));
 
 
         List<WebElement> cities = driver.findElements(By.xpath("//ul[@class='react-autosuggest__suggestions-list']/li"));
         for (WebElement e : cities) {
-
+            //
+            JVMwait(2000);
             if (e.getText().contains("Mumbai")) {
+
                 e.click();
+                break;
             }
         }
+        JVMwait(3000);
         ////input[@placeholder='To']
-        WebElement tocity= driver.findElement(By.id("toCity"));
+        WebElement tocity = driver.findElement(By.id("toCity"));
         act.moveToElement(tocity).click(tocity)
                 .pause(Duration.ofSeconds(1))
                 .sendKeys("BLR")
                 .build()
                 .perform();
-        List<WebElement> tocities= driver.findElements(By.xpath("//li[@class='react-autosuggest__suggestion']/div"));
-        for (WebElement f:tocities){
-            if (f.getText().contains("Bangalore")){
+        checkvisibility(driver, By.xpath("//li[@class='react-autosuggest__suggestion']/div"));
+        List<WebElement> tocities = driver.findElements(By.xpath("//li[@class='react-autosuggest__suggestion']/div"));
+        for (WebElement f : tocities) {
+
+            if (f.getText().contains("Bengaluru")) {
                 f.click();
+                break;
+            }
+        }
+        ////div[@class='DayPicker-Day']
+        checkvisibility(driver, By.xpath("//div[contains(@class, 'DayPicker-Caption')]/div"));
+        List<WebElement> months = driver.findElements(By.xpath("//div[contains(@class, 'DayPicker-Caption')]/div"));
+        for (WebElement month : months) {
+            WebElement caption = month.findElement(By.xpath("//div[@class='DayPicker-Caption']/div"));
+            String monthText = caption.getText();
+            if (monthText.contains("May")) {
+                List<WebElement> days = month.findElements(By.xpath("//div[@class='dateInnerCell']/p"));
+                for (WebElement day : days) {
+                    String dayText = day.getText();
+                    if (dayText.contains("15")) {
+                        day.click();
+                        break;
+
+                    }
+                }
+
+            } else {
+                WebElement nextmonth = driver.findElement(By.xpath("//span[@aria-label='Next Month']"));
+                nextmonth.click();
             }
         }
 
+
+        checkvisibility(driver, By.xpath("//p[@data-cy='submit']/a"));
+        WebElement submitbtn = driver.findElement(By.xpath("//p[@data-cy='submit']/a"));
+        submitbtn.click();
+        System.out.println(driver.getCurrentUrl());
+
+        JVMwait(3000);
 
 
     }
